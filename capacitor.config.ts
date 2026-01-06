435067e59f198162a9f1c90b87e0a1ee259742ee
boerdb <75579276+boerdb@users.noreply.github.com>
2026-01-06 16:54:00 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ic-bieb',
+  webDir: 'www'
+};
+
+export default config;
